a62f8936fd26bfefa273a020b79e0062aaf7cf2b
DanikNik <sway9009>
2019-02-07 13:15:43 +0300

    minor fixes

--- source/chess.js
+++ source/chess.js
@@ -1,11 +1,11 @@
 'use strict';
 
 function chess(n) {
-    if (n == 1) {
+    if (parseInt(n) === 1) {
         return null;
     }
-    var ret = "";
-    var was_black_start = false;
+    let ret = "";
+    let was_black_start = false;
     for (let i = 0; i < parseInt(n); i++) {
         let was_black = was_black_start;
         for (let j = 0; j < parseInt(n); j++) {

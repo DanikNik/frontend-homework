4eb32c94b5b2af52ec6e8958581d10affb3b6d4b
DanikNik <sway9009>
2019-02-08 15:20:15 +0300

    переписал на стринговых методах

--- source/chess.js
+++ source/chess.js
@@ -8,26 +8,33 @@
  * If layout is impossible - returns null
  */
 
+
+
+
 function chess(n) {
 
-    if (isNaN(parseInt(n))){
+    if (Number.isNaN(parseInt(n))) {
         return null
     }
 
     if (parseInt(n) <= 1) {
         return null;
     }
-    let ret = String();
-    let was_black_start = false;
-    for (let i = 0; i < parseInt(n); i++) {
-        let was_black = was_black_start;
-        for (let j = 0; j < parseInt(n); j++) {
-            ret += was_black ? " " : "*";
-            was_black = !was_black;
+    let raw_string = ''.padEnd(n * n, "* ");
+    let re = new RegExp('.{' + n + '}', 'g');
+
+
+    let reverse_str = str => str.split("").reverse().join("");
+    let string_arr = raw_string.match(re);
+    if (n % 2 === 0) {
+        for (let i = 0; i < n; i ++){
+            if (i%2 !== 0){
+                string_arr[i] = reverse_str(string_arr[i]);
+            }
         }
-        ret += "\n";
-        was_black_start = !was_black_start;
     }
-    return ret;
+
+    return string_arr.join('\n') + '\n';
 }
 
+console.log(chess(3));
